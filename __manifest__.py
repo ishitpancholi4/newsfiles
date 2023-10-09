14913dddb22afebfaa4a11cddc5a541dd4fd1d33
ishit <ishit@gmail.com>
2023-10-09 16:34:54 +0530

    first

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+  'name': 'School Management',
+  'version': '15.0.1.0.0',
+  'category': '',
+  'author': 'Cybrosys Techno Solutions',
+  'website': "https://www.cybrosys.com",
+  'company': 'Cybrosys Techno Solutions',
+  'maintainer': 'Cybrosys Techno Solutions',
+  'summary': 'Record Student Details',
+  "description": """To manage School""",
+  'depends': ['base'],
+   'data': [
+   ],
+  'qweb': [],
+  "license": "AGPL-3",
+  'installable': True,
+  'auto_install': False,
+}
---
+++ snippet/custom/ishit_management/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-pack
+{  # App information
+    'name': 'Ishit Management',
+    'category': '',
+    'version': '16.0',
+    'summary': """ Managments are made up here""",
+    'description': """ Module is for managing office """,
+    'depends': [],
+
+    'data': ['security/ir.model.access.csv',
+             'views/management_view.xml',
+             #'views/menu_view.xml',
+             ],
+
+    'images': [],
+    # 'images': ['static/description/christmas.gif'],
+    'author': 'Vraja Technologies',
+    'maintainer': 'Vraja Technologies',
+    'website': 'https://www.vrajatechnologies.com',
+    'live_test_url': 'https://www.vrajatechnologies.com/contactus',
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    # 'price': '101',
+    # 'currency': 'EUR',
+    'license': 'LGPL-3',
+}
